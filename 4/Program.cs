7a2c203131b8aa15a5678b0fbbbe17b3745dfd0b
Evgenii <w1djer@mail.ru>
2023-03-06 01:22:41 +0300

    четвертая задача

---
+++ 4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+if(N > 0)
+{
+    for(int i = 2; i < N; i = i + 2)
+        Console.Write(i +" ");
+}
+else
+{
+    for(int i = -2; i > N; i = i - 2)
+        Console.Write(i +" ");
+}
