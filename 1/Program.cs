8c0e7193694b313ad4c6ff34a8c5879cb93a37ec
Evgenii <w1djer@mail.ru>
2023-03-06 00:13:01 +0300

    первая задача

---
+++ 1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите первое число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(x > b)
+{
+    Console.WriteLine($"Меньшее число: {b}");
+    Console.Write($"Большее число: {x}");
+}
+else
+{
+    Console.WriteLine($"Меньшее число: {x}");
+    Console.Write($"Большее число: {b}");
+}
