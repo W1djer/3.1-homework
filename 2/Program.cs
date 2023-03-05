7efb770846d14af1f791c4fb8163a58e06c85189
Evgenii <w1djer@mail.ru>
2023-03-06 00:39:51 +0300

    вторая задача

---
+++ 2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите 3 числа:");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+
+if(a > b)
+    max = a;
+else
+    max = b;
+
+if(max > c)
+    Console.Write($"Максимальное {max}");
+else
+{
+    max = c;
+    Console.Write($"Максимальное {max}");
+}
